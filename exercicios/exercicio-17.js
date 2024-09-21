a836c73b2d1210b50c61530a196189e54a476bcb
PAULO SERGIO ESTEVES <PSESTEVESVR@GMAIL.COM>
2024-09-21 16:17:44 -0300

    Atualizações do Exercicios

--- exercicios/exercicio-17.js
+++ exercicios/exercicio-17.js
@@ -4,12 +4,12 @@ function gerador(){
     var tab = document.getElementById('itab')
 
     if (n.value.length == 0){
-        window.alert('[ERRO!] Favor adcionar um número [Natural]!')
+        window.alert('[ERRO!] Favor adcionar um número!')
     }else{
         var n = Number(n.value)
         var c = 1
         tab.innerHTML = ''
-        if (n >= 0){
+        if (n >= 0 || n <= 0){
             for (i = 1;i <= 10;i++){
                 var m = 0 * i
                 var item = document.createElement('option')
@@ -17,7 +17,7 @@ function gerador(){
                 tab.appendChild(item)
                 tab.style.backgroundColor = '#68c8f2'
                 tab.style.borderRadius = '5px'
-                item.style.textShadow = '2px 2px 2px black'
+                item.style.textShadow = '1px 1px 2px black'
             }
         }
    }
