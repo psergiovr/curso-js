22c25e1a7a86f2ed04135b20dcd9bf8da59ea09d
PAULO SERGIO ESTEVES <PSESTEVESVR@GMAIL.COM>
2024-09-27 19:24:41 -0300

    Atualizações de Analisador de Num

---
+++ variáveis.compostas/verificar.js
@@ -0,0 +1,47 @@
+    let num = document.querySelector('input#inum')
+    let tab = document.querySelector('select#ilista')
+    let valor = []
+
+function nalista(num,lis){
+    if(lis.indexOf(Number(num)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+     
+    if(num.value.length == 0 || nalista(num.value, valor)){
+        window.alert('[ERRO!] Verifique os dados!')
+    }
+    else{
+        valor.push(Number(num.value)) 
+        
+        let item = document.createElement('option')
+        item.text = `Número ${num.value} adicionado!`
+        tab.appendChild(item)
+        res.innerHTML = ''
+    }
+        num.value = ''
+        num.focus()
+}
+function verificando(){
+    
+    if(valor.length == 0){
+        window.alert('[ERRO!!] Verifique os dados!')
+    }
+    else {
+        let soma = 0
+        let media = 0
+        for(let pos in valor){
+            soma += valor[pos]
+        }
+        media = soma/valor.length
+        res.innerHTML = ``
+        res.innerHTML = `<p>A soma de todos os valores é igual a: ${soma}</p>`
+        res.innerHTML += `<p>Ao todo temos ${valor.length} números cadastrados.</p>`
+        res.innerHTML += `<p>A média dos valores é igual a: ${media}</p>`
+    }
+        
+}
